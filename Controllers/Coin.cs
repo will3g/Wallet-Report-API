3fbe4593de508874d43e0212dfd251f13794fb72
will3g <willianpaulo_09@hotmail.com>
2020-09-14 20:50:40 -0300

    first commit

---
+++ Controllers/Coin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System.Net.Http;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Coin : ControllerBase
+    {
+        [HttpGet("{coin}")]
+        public async Task<IActionResult> CoinTask(string coin)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("https://brasilbitcoin.com.br");
+                    var response = await client.GetAsync($"/API/prices/{coin}");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<CoinModel>(stringResult);
+
+                    return Ok(new
+                    {
+                        Last = rawWeather.Last,
+                        Max = rawWeather.Max,
+                        Min = rawWeather.Min,
+                        Buy = rawWeather.Buy,
+                        Sell = rawWeather.Sell,
+                        Open = rawWeather.Open,
+                        Vol = rawWeather.Vol,
+                        Trade = rawWeather.Trade,
+                        Trades = rawWeather.Trades,
+                        Vwap = rawWeather.Vwap,
+                        Money = rawWeather.Money,
+                    });
+                 }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ Models/CoinModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CoinModel
+    {
+        [JsonProperty("last")]
+        public double Last { get; set; }
+
+        [JsonProperty("max")]
+        public double Max { get; set; }
+
+        [JsonProperty("min")]
+        public double Min { get; set; }
+
+        [JsonProperty("buy")]
+        public double Buy { get; set; }
+
+        [JsonProperty("sell")]
+        public double Sell { get; set; }
+
+        [JsonProperty("open")]
+        public double Open { get; set; }
+
+        [JsonProperty("vol")]
+        public double Vol { get; set; }
+
+        [JsonProperty("trade")]
+        public double Trade { get; set; }
+
+        [JsonProperty("trades")]
+        public int Trades { get; set; }
+
+        [JsonProperty("vwap")]
+        public double Vwap { get; set; }
+
+        [JsonProperty("money")]
+        public double Money { get; set; }
+    }
+}

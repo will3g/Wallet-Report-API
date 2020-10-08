ced27d8978470af0bf4a79b1133e5e6d56a2f281
tiagoanderson7 <66435544+tiagoanderson7@users.noreply.github.com>
2020-10-08 16:00:46 -0300

    all_user_contain

---
+++ Controllers/User.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Data.Common;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class User : ControllerBase
+    {
+        public User(AppDb db)
+        {
+            Db = db;
+        }
+
+        // GET api/user/{CPF}
+        [HttpGet("/api/user/{CPF}")]
+        public async Task<IActionResult> GetLatest(string CPF)
+        {
+            await Db.Connection.OpenAsync();
+            var query = new UserPostQuery(Db);
+            var result = await query.LatestPostsAsync(CPF);
+            return new OkObjectResult(result);
+        }
+
+        // GET ALL api/user/
+        [HttpGet("/api/user/")]
+        public async Task<IActionResult> GetAll()
+        {
+            await Db.Connection.OpenAsync();
+            var query = new UserPostQuery(Db);
+            var result = await query.AllPostsAsync();
+            return new OkObjectResult(result);
+        }
+
+        // POST api/user/
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserModel body)
+        {
+            await Db.Connection.OpenAsync();
+            body.Db = Db;
+            await body.InsertAsync();
+            return new OkObjectResult(body);
+        }
+
+        // PUT api/user/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOne(int id, [FromBody] UserModel body)
+        {
+            await Db.Connection.OpenAsync();
+            var query = new UserPostQuery(Db);
+            var result = await query.FirstPostAsync(id);
+            if (result is null)
+                return new NotFoundResult();
+            result.CPF = body.CPF;
+            result.Nome = body.Nome;
+            result.Senha = body.Senha;
+            result.Email = body.Email;
+            await result.UpdateAsync();
+            return new OkObjectResult(result);
+        }
+
+        // DELETE api/user/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOne(int id)
+        {
+            await Db.Connection.OpenAsync();
+            var query = new UserPostQuery(Db);
+            var result = await query.FirstPostAsync(id);
+            if (result is null)
+                return new NotFoundResult();
+            await result.DeleteAsync();
+            return new OkResult();
+        }
+
+        // DELETE ALL api/user/
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAll()
+        {
+            await Db.Connection.OpenAsync();
+            var query = new UserPostQuery(Db);
+            await query.DeleteAllAsync();
+            return new OkResult();
+        }
+
+        public AppDb Db { get; }
+    }
+}
---
+++ Controllers/UserPostQuery.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace WebApplication2
+{
+    public class UserPostQuery
+    {
+        public AppDb Db { get; }
+
+        public UserPostQuery(AppDb db)
+        {
+            Db = db;
+        }
+
+        public async Task<List<UserModel>> AllPostsAsync()
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"SELECT * FROM `user`;";
+            return await ReadAllAsync(await cmd.ExecuteReaderAsync());
+        }
+
+        public async Task<UserModel> FirstPostAsync(int id)
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"SELECT * FROM `user` WHERE `Id` = @id;";
+            cmd.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = "@id",
+                DbType = DbType.Int32,
+                Value = id,
+            });
+            var result = await ReadAllAsync(await cmd.ExecuteReaderAsync());
+            return result[0];
+        }
+
+        public async Task<List<UserModel>> LatestPostsAsync(string CPF)
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"SELECT * FROM `user` WHERE `CPF` = @CPF;";
+            cmd.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = "@CPF",
+                DbType = DbType.String,
+                Value = CPF,
+            });
+         
+            return await ReadAllAsync(await cmd.ExecuteReaderAsync());
+        }
+
+        public async Task DeleteAllAsync()
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"DELETE FROM `user`";
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        private async Task<List<UserModel>> ReadAllAsync(DbDataReader reader)
+        {
+            var posts = new List<UserModel>();
+            using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+                    var post = new UserModel(Db)
+                    {
+                        Id = reader.GetInt32(0),
+                        CPF = reader.GetString(1),
+                        Nome = reader.GetString(2),
+                        Senha = reader.GetString(3),
+                        Email = reader.GetString(4),
+                    };
+                    posts.Add(post);
+                }
+            }
+            return posts;
+        }
+
+        internal Task ReadAllAsync(ClaimsPrincipal user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

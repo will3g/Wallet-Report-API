a0b12f8e21897ac78f94ad4a0da1cdfe9c76b38d
will3g <willianpaulo_09@hotmail.com>
2020-10-11 13:30:58 -0300

    rename-config-database-and-remove-database-name

--- Startup.cs
+++ Startup.cs
@@ -18,7 +18,7 @@ namespace WebApplication2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<AppDb>(_ => new AppDb(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddTransient<AppDb>(_ => new AppDb(Configuration["ConnectionStrings:WRConnection"]));
             services.AddControllers();
         }
 
